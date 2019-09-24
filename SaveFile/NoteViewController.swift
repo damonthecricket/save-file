5f9be7c3e6bab18002885ada3dda6b540c7390ef
Damon Cricket <d.a.tsvirko@gmail.com>
2019-09-24 17:39:37 +0200

    Complete.

--- SaveFile/NoteViewController.swift
+++ SaveFile/NoteViewController.swift
@@ -11,10 +11,8 @@ import UIKit
 // MARK: - NoteViewController
 
 class NoteViewController: UIViewController {
-    
-    @IBOutlet weak var saveBarButtonItem: UIBarButtonItem?
-    
-    @IBOutlet weak var textField: UITextField?
+
+    @IBOutlet weak var textView: UITextView?
     
     var noteName = ""
     
@@ -25,12 +23,27 @@ class NoteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = noteName
         
+        let url = fileURL()
+        textView?.text = try? String(contentsOf: url, encoding: .utf8)
     }
     
     // MARK: - UIActions
     
     @IBAction func saveBarButtonTap(sender: UIBarButtonItem) {
-        
+        do {
+            let text = textView!.text
+            let url = fileURL()
+            try text?.write(to: url, atomically: false, encoding: .utf8)
+        } catch let error {
+            print("File save error \(error.localizedDescription)")
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func fileURL() -> URL {
+        return folderURL.appendingPathComponent(noteName)
     }
 }
